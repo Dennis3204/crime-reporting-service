cb83857e578cd5cec4847830c4218e682e01d145
Elianfer <efernand1@stevens.edu>
2025-12-10 14:28:58 -0500

    Profile & Authentication
    Added profile page, address fields, and auth redirect if logged in or logged out.

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,15 +4,22 @@ import { RenderableError } from "../helpers/errors.js";
 
 const router = Router();
 
-router.get("/signup", (req, res) => {
+const redirectIfLoggedIn = (req, res, next) => {
+  if (req.session.user) {
+      return res.redirect("/");
+  }
+  next();
+};
+
+router.get("/signup", redirectIfLoggedIn, (req, res) => {
     res.render("signup", { title: "Sign Up" });
 });
 
 router.post("/signup", async (req, res) => {
-    let { username, first_name, last_name, age, password } = req.body;
+    let { username, first_name, last_name, age, password, street, city, state, zipcode } = req.body;
     try {
-        const newUser = await createUser(username, first_name, last_name, age, password);
-        req.session.user = {_id: newUser._id,username: newUser.username};
+        const newUser = await createUser(username, first_name, last_name, age, password, street, city, state, zipcode);
+        req.session.user = {_id: newUser._id,username: newUser.username, address: newUser.address};
         return res.redirect("/");
     } catch (e) {
         const code = e instanceof RenderableError ? e.code : 500;
@@ -20,7 +27,7 @@ router.post("/signup", async (req, res) => {
     }
 });
 
-router.get("/login", (req, res) => {
+router.get("/login", redirectIfLoggedIn, (req, res) => {
     res.render("login", { title: "Login" });
 });
 
@@ -28,7 +35,7 @@ router.post("/login", async (req, res) => {
     let { username, password } = req.body;
     try {
         const user = await checkUser(username, password);
-        req.session.user = {_id: user._id,username: user.username};
+        req.session.user = {_id: user._id,username: user.username, address: user.address};
         return res.redirect("/");
     } catch (e) {
         const code = e instanceof RenderableError ? e.code : 500;
@@ -40,5 +47,6 @@ router.get("/logout", (req, res) => {
     req.session.destroy();
     res.redirect("/");
 });
+
 
 export default router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import homeRoutes from "./home.js";
 import reportsRoutes from "./reports.js";
 import authRoutes from "./auth.js";
+import profileRoutes from "./profile.js";
 import * as errors from "../helpers/errors.js";
 
 const constructorMethod = (app) => {
   app.use("/", authRoutes);
   app.use("/", homeRoutes);
+  app.use("/profile", profileRoutes);
   app.use("/reports", reportsRoutes);
   app.use("/public", express.static("public"));
   app.use("{*splat}", (req, res) => {
