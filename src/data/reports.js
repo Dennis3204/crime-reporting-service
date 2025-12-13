599fd9d65df7333644a5ee092ae617126e629892
Pranav Chaudhari <pchaudha1@stevens.edu>
2025-12-13 12:01:00 -0500

    created path to uploaded image

--- src/data/reports.js
+++ src/data/reports.js
@@ -110,7 +110,6 @@ export const searchByKeyword = async (keyword) =>{
     let key_regex = new RegExp('.*' + keyword + '.*', 'i')
     const report = await collections.reports();
     const result = await report.find({title: {$regex: key_regex}}).toArray()
-    console.log(result)
     if(result.length === 0)
       throw "404"
     return result
@@ -120,7 +119,6 @@ export const searchByCrime = async (keyword) =>{
     let key_regex = new RegExp('.*' + keyword + '.*', 'i')
     const report = await collections.reports();
     const result = await report.find({crime: {$regex: key_regex}}).toArray()
-    console.log(result)
     if(result.length === 0)
       throw "404"
     return result
