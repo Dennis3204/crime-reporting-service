f617015a84ae6905c1798eba462dbd86d825e6fe
Pranav Chaudhari <pchaudha1@stevens.edu>
2025-12-13 14:54:30 -0500

    clien-validation to search

---
+++ public/js/search.js
@@ -0,0 +1,23 @@
+const search_form = document.getElementById('search_form')
+if(search_form){
+    let keyword_element = document.getElementById("keyword")
+    let target_element = document.getElementById('target')
+    let error_div = document.getElementById('error_block')
+    error_div.hidden = true
+    search_form.addEventListener('submit',(event)=>{
+        keyword = keyword_element.value
+        target = target_element.value
+        try{
+            error_div.hidden = true
+            if(keyword.trim().length === 0)
+                throw "Please enter a keyword"
+            if(target !== "title" && target !== "crime" && target !== "zipcode")
+                throw "Ivalid Category"
+        }catch(e){
+            event.preventDefault()
+            const message = typeof e === 'string' ? e : e.message;
+            error_div.textContent = message
+            error_div.hidden = false
+        }
+    })
+}
