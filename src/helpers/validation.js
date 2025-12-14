75e48068dbfc090b7336e4b537a7c94ffa6ad0cb
Thomas Kain <tkain@stevens.edu>
2025-12-13 22:52:27 -0500

    Remove redundant zipcode validation function

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -7,15 +7,6 @@ const InvalidInputError = class extends BadRequestError {
   }
 };
 
-export const validateZip = (zip, name = "ZIP code") => {
-  zip = validateTrimmableString(zip, name);
-
-  if (!/^\d{5}$/.test(zip)) {
-    throw new InvalidInputError(`${name} must be a 5-digit number.`);
-  }
-  return zip;
-};
-
 export const validateString = (str, name) => {
   if (typeof str !== "string" || str.length === 0)
     throw new InvalidInputError(`Expected ${name} to be a non-empty string.`);
